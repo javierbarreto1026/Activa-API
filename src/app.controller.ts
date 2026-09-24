2c5323451c0605ef844b00920081bad1469476eb
JavierBarreto <javierbarreto1026@gmail.com>
2026-09-24 10:54:17 -0500

    feat: crear interfaz de login y validación de credenciales mockeadas en backend

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post, Body, Redirect, UnauthorizedException } from '@nestjs/common';
+
+// 1. Creamos el "molde" estricto para evitar el uso de 'any'
+interface LoginDto {
+  username?: string;
+  password?: string;
+}
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Redirect('/login.html', 302)
+  raiz() {}
+
+  @Post('api/login')
+  login(@Body() datosDelFormulario: LoginDto) {
+    const username = datosDelFormulario.username || '';
+    const password = datosDelFormulario.password || '';
+
+    const usuarioValido = 'admin@empresa.com';
+    const passwordValida = '123456';
+
+    if (username === usuarioValido && password === passwordValida) {
+      return {
+        success: true,
+        mensaje: '¡Bienvenido!',
+        usuario: username,
+      };
+    }
+
+    throw new UnauthorizedException('Credenciales incorrectas');
   }
 }
