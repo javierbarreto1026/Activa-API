067f013f7c261425901510f9132bf005c893e624
JavierBarreto <javierbarreto1026@gmail.com>
2026-09-24 10:46:32 -0500

    feat: configurar servidor de archivos estáticos

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DocumentsModule } from './documents/documents.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [DocumentsModule],
+  imports: [
+    DocumentsModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
